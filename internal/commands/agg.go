ec9770838ab5444a932290a541c7951a7a7fc836
MoisesASantos <moisessantosa23@gmail.com>
2026-06-26 17:44:10 +0100

    update the agg commant to be loop that still fecthing the feeds on database, now recieves a time arg to apoint the interval time that we want to fecth a feed

--- internal/commands/agg.go
+++ internal/commands/agg.go
@@ -3,18 +3,48 @@ package commands
 import (
 	"fmt"
 	"context"
+	"time"
 	"github.com/MoisesASantos/BLOG-AGGREGATOR/internal/rss"
 )
 
-func HandlerAgg(s *State, cmd Command) error {
+func scrapeFeeds(s *State) error {
 
 	ctx := context.Background()
-	
-	data, err := rss.FetchFeed(ctx, "https://www.wagslane.dev/index.xml")
+
+	feed, err := s.Db.GetNextFeedToFetch(ctx)
 	if err != nil {
-		fmt.Println("Error trying to get a fetch")
+		return fmt.Errorf("get next feed: %w", err)
+	}
+	err = s.Db.MarkFeedFetched(ctx, feed.ID)
+	if err != nil {
+		return fmt.Errorf("mark fetched: %w", err)
+	}
+
+	data, err := rss.FetchFeed(ctx, feed.Url)
+	if err != nil {
+		return fmt.Errorf("fetch feed: %w", err)
+	}
+	fmt.Println("ITEMS:", len(data.Channel.Item))
+	
+	for _, item := range data.Channel.Item {
+		fmt.Println(item.Title)
+	}
+
+	return nil
+}
+
+func HandlerAgg(s *State, cmd Command) error {
+
+	timeBetweenRequests, err := time.ParseDuration(cmd.Args[0])
+	if err != nil {
+		fmt.Printf("failed to convert time with ParseDuration on HandlerAgg function: %v\n", err)
 		return err
 	}
-	fmt.Println(data)
+
+	ticker := time.NewTicker(timeBetweenRequests)
+	
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
 	return nil
 }
--- internal/rss/rss.go
+++ internal/rss/rss.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"time"
 	"html"
+	//"bytes"
 )
 
 type RSSFeed struct {
@@ -36,28 +37,31 @@ func FetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
 
 	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
 	if err != nil {
-		fmt.Println("Error to make a request")
-		return nil, errors.New("Error to make a request")
+    	return &RSSFeed{}, fmt.Errorf("Error to make a request: %w", err)
 	}
 	req.Header.Set("User-Agent", "gator")
 
 	res, err := client.Do(req)
 	if err != nil {
-		fmt.Println("Error to make a request")
-		return &RSSFeed{}, errors.New("Error to make a request")
+		fmt.Println("Pau dos paus")
+    	return &RSSFeed{}, fmt.Errorf("Error to make a request: %w", err)
 	}
 	defer res.Body.Close()
+	fmt.Println("Status:", res.Status)
+
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		fmt.Println("Error to try read the body response")
 		return &RSSFeed{}, errors.New("Error to try read the body response")
 	}
-
+	
+	/*fmt.Println(string(body[:300]))
+	decoder := xml.NewDecoder(bytes.NewReader(body))
+	decoder.Strict = false*/
 	err = xml.Unmarshal(body, &data)
 	if err != nil {
-		fmt.Println("Error to try unmarshal the xml content")
-		return &RSSFeed{}, errors.New("Error to try unmarshal the xml content")
+    	return &RSSFeed{}, fmt.Errorf("xml unmarshal failed: %w", err)
 	}
 
 	data.Channel.Title = html.UnescapeString(data.Channel.Title)
